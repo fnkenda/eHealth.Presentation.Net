7a4dbfc31cf06c84cabe78067af38578cb237a1b
fnkenda <fnkenda@hotmail.com>
2024-01-25 23:19:23 +0100

    Amélioration de la liste des patients

--- obj/Debug/net8.0/eHealth.Presentation.AssemblyInfo.cs
+++ obj/Debug/net8.0/eHealth.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eHealth.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f0652f8da727bc0019c11ec2074ae53c1043bdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("eHealth.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eHealth.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
