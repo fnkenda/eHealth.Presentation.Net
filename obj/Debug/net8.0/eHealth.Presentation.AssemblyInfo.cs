924718ffc7162a022b7275c6acdcb3c424571606
fnkenda <fnkenda@hotmail.com>
2024-01-26 14:58:38 +0100

    Untracked bin, obj and LogFile in for Clean version

--- obj/Debug/net8.0/eHealth.Presentation.AssemblyInfo.cs
+++ obj/Debug/net8.0/eHealth.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eHealth.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a4dbfc31cf06c84cabe78067af38578cb237a1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4535fe853c9b919c45a1138ff6b543273f6200")]
 [assembly: System.Reflection.AssemblyProductAttribute("eHealth.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eHealth.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
